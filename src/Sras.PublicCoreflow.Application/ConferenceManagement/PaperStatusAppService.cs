97e58c2d386542727ed0136e7ad29c23026b68f0
HoangDuong1106 <maihoangduong11062000@gmail.com>
2023-06-10 12:04:32 +0700

    GET-ALL-PAPER-STATUS API

---
+++ src/Sras.PublicCoreflow.Application/ConferenceManagement/PaperStatusAppService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sras.PublicCoreflow.ConferenceManagement
+{
+    public class PaperStatusAppService : PublicCoreflowAppService, IPaperStatusAppService
+    {
+
+        private readonly IPaperStatusRepository _paperStatusRepository;
+        public PaperStatusAppService(IPaperStatusRepository paperStatusRepository) {
+            _paperStatusRepository = paperStatusRepository;
+        }
+
+        public async Task<IEnumerable<object>> GetAllPaperStatusAsync()
+        {
+            return await _paperStatusRepository.GetAllPaperStatus();
+        }
+
+    }
+}
---
+++ src/Sras.PublicCoreflow.EntityFrameworkCore/EntityFrameworkCore/ConferenceManagement/PaperStatusRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sras.PublicCoreflow.ConferenceManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace Sras.PublicCoreflow.EntityFrameworkCore.ConferenceManagement
+{
+    public class PaperStatusRepository : EfCoreRepository<PublicCoreflowDbContext, Outsider, Guid>,  IPaperStatusRepository
+    {
+        public PaperStatusRepository(IDbContextProvider<PublicCoreflowDbContext> dbContextProvider) : base(dbContextProvider)
+        {
+        }
+
+        public async Task<IEnumerable<object>> GetAllPaperStatus()
+        {
+            var dbContext = await GetDbContextAsync();
+            var result = await dbContext.PaperStatuses.Select(p => new
+            {
+                statusId = p.Id,
+                statusName = p.Name
+            }).ToListAsync();
+
+            return result;
+        }
+    }
+}
