70a16725acf7cad05adf1619ee1089cca56c699f
HoangDuong1106 <maihoangduong11062000@gmail.com>
2023-06-13 21:43:20 +0700

    Update Step 4 API

--- src/Sras.PublicCoreflow.Domain.Shared/Dto/PaperStatusIdAndTemplateId.cs
+++ src/Sras.PublicCoreflow.Domain.Shared/Dto/PaperStatusIdAndTemplateId.cs
@@ -6,7 +6,7 @@ namespace Sras.PublicCoreflow.Dto
 {
     public class PaperStatusIdAndTemplateId
     {
-        public Guid paperStatusId { get; set; }
+        public Guid statusId { get; set; }
         public Guid templateId { get; set; }
     }
 }
