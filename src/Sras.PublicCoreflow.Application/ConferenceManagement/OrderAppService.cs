21f037aa81508b159bd5e7c0c902d62368fbfa82
HoangDuong1106 <maihoangduong11062000@gmail.com>
2023-06-16 05:37:17 +0700

    Add get-order-detail API

---
+++ src/Sras.PublicCoreflow.Application/ConferenceManagement/OrderAppService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sras.PublicCoreflow.ConferenceManagement
+{
+    public class OrderAppService : PublicCoreflowAppService, IOrderAppService
+    {
+        private readonly IOrderRepository orderRepository;
+
+        public OrderAppService(IOrderRepository orderRepository)
+        {
+            this.orderRepository = orderRepository;
+        }
+
+        public async Task<object> GetOrderDetail(Guid orderId)
+        {
+            return await orderRepository.GetOrderDetail(orderId);
+        }
+    }
+}
---
+++ src/Sras.PublicCoreflow.EntityFrameworkCore/EntityFrameworkCore/ConferenceManagement/OrderRepository.cs
@@ -0,0 +1,26 @@
+﻿using Sras.PublicCoreflow.ConferenceManagement;
+using System;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace Sras.PublicCoreflow.EntityFrameworkCore.ConferenceManagement
+{
+    public class OrderRepository : EfCoreRepository<PublicCoreflowDbContext, Email, Guid>, IOrderRepository
+    {
+        public OrderRepository(IDbContextProvider<PublicCoreflowDbContext> dbContextProvider) : base(dbContextProvider)
+        {
+        }
+
+        public async Task<object> GetOrderDetail(Guid orderId)
+        {
+            var dbContext = await GetDbContextAsync();
+            var result = dbContext.Orders.Where(o => o.Id == orderId).First().OrderDetails;
+
+            return JsonSerializer.Deserialize<OrderDto>(result);
+        }
+    }
+}
