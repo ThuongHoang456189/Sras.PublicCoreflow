4f6e9e5f203f579d458d0db7fbe98cb3eb613fee
Thuong Hoang <ngocanh654189@gmail.com>
2023-06-18 12:15:23 +0700

    hoangthuong-18-6-2023_Update Migration29

---
+++ src/Sras.PublicCoreflow.EntityFrameworkCore/Migrations/20230618051413_Migration29.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sras.PublicCoreflow.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration29 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TempFilePath",
+                table: "Websites",
+                type: "nvarchar(1024)",
+                maxLength: 1024,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TempFilePath",
+                table: "Websites");
+        }
+    }
+}
