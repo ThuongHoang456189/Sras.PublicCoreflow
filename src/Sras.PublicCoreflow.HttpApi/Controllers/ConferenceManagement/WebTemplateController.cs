82580aa28b5b2b9c58da949a278d9ad0df34667c
HoangDuong1106 <maihoangduong11062000@gmail.com>
2023-06-23 04:25:17 +0700

    Download file zip have all template

--- src/Sras.PublicCoreflow.HttpApi/Controllers/ConferenceManagement/WebTemplateController.cs
+++ src/Sras.PublicCoreflow.HttpApi/Controllers/ConferenceManagement/WebTemplateController.cs
@@ -3,7 +3,11 @@ using Microsoft.CodeAnalysis;
 using Sras.PublicCoreflow.ConferenceManagement;
 using Sras.PublicCoreflow.Dto;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Net.Mime;
 using System.Threading.Tasks;
@@ -63,12 +67,39 @@ namespace Sras.PublicCoreflow.Controllers.ConferenceManagement
         }
 
         [HttpGet("download-all-templates")]
-        public ActionResult downloadAllTemplates()
+        public async Task<ActionResult> downloadAllTemplates()
         {
-            var filedata = _webTemplateAppService.downloadAllTemplates();
-            var filename = "webTemplate";
+            IEnumerable<byte[]> listBytes = await _webTemplateAppService.downloadAllTemplates();
+            using (var ms = new MemoryStream())
+            {
+                using (var archive =
+                new System.IO.Compression.ZipArchive(ms, ZipArchiveMode.Create, true))
+                {
 
-            return File(filedata.Result.First(), MediaTypeNames.Text.Plain, "WebTemplate.html");
+                    var zipEntry = (ZipArchiveEntry)null;
+                    foreach (var (item, index) in listBytes.Select((value, i) => (value, i)))
+                    {
+                        zipEntry = archive.CreateEntry("template" + index + ".html", CompressionLevel.Fastest);
+                        using (var zipStream = zipEntry.Open())
+                        {
+                            zipStream.Write(item, 0, item.Length);
+                        }
+                    }
+
+                    //var zipEntry = archive.CreateEntry("image1.png", CompressionLevel.Fastest);
+                    //using (var zipStream = zipEntry.Open())
+                    //{
+                    //    zipStream.Write(bytes1, 0, bytes1.Length);
+                    //}
+
+                    //var zipEntry2 = archive.CreateEntry("image2.png", CompressionLevel.Fastest);
+                    //using (var zipStream = zipEntry2.Open())
+                    //{
+                    //    zipStream.Write(bytes2, 0, bytes2.Length);
+                    //}
+                }
+                return File(ms.ToArray(), "application/zip", "Template.zip");
+            }
         }
 
     }
