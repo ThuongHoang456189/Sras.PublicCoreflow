0e09d9090d5d5c65b7ababe859b29bb346d6bf19
HoangDuong1106 <maihoangduong11062000@gmail.com>
2023-07-11 23:26:34 +0700

    change AcademicDegreeId to int

--- src/Sras.PublicCoreflow.Domain.Shared/Dto/Education.cs
+++ src/Sras.PublicCoreflow.Domain.Shared/Dto/Education.cs
@@ -5,7 +5,7 @@ namespace Sras.PublicCoreflow.ConferenceManagement
     public class Education
     {
         public Guid EducationId { get; set; }
-        public Guid AcademicDegreeId { get; set; }// - Bac dao tao academic-degree-level-reference-types json
+        public int AcademicDegreeId { get; set; }// - Bac dao tao academic-degree-level-reference-types json
         public Organization EducationalOrganization { get; set; } // - Noi dao tao
         public int StartYear { get; set; } // Nam bat dau
         public int YearOfGraduation { get; set; } // Nam tot nghiep
