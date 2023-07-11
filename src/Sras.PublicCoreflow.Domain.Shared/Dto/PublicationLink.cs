746d4e1134b8050c828ef5aa951b17d727c4190f
HoangDuong1106 <maihoangduong11062000@gmail.com>
2023-07-11 23:43:53 +0700

    change publicationlink Id to Guid

--- src/Sras.PublicCoreflow.Domain.Shared/Dto/PublicationLink.cs
+++ src/Sras.PublicCoreflow.Domain.Shared/Dto/PublicationLink.cs
@@ -2,7 +2,7 @@
 {
     public class PublicationLink
     {
-        public int Id { get; set; }
+        public System.Guid Id { get; set; }
         public string Label { get; set; }
         public string Link { get; set; }
     }
